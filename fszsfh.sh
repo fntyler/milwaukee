b2fd9082a9c233a30eb55ae9729a9945e167c7f6
fntyler <71953103+fntyler@users.noreply.github.com>
2024-11-26 23:03:54 -0500

    feat: major
    refactor logging
    
    configure logic to behave as originally intended

--- fszsfh.sh
+++ fszsfh.sh
@@ -2,33 +2,40 @@
 
 # WIP
 # ssh sessionizer using tmux
+# inspired by https://github.com/ThePrimeagen/tmux-sessionizer/blob/master/tmux-sessionizer
 
-function _log() {
-    local DATE
-    local LOG_LEVEL
-    local LOG_FILE
-    local LOG_MESSAGE
-
-    DATE=$(date '+%Y-%m-%d %T')
-    LOG_LEVEL=${1:-'INFO'}
-    LOG_FILE=${2:-/tmp/fszsfh.log}
-
-    read -r LOG_MESSAGE
-    echo "$DATE [$LOG_LEVEL] ${LOG_MESSAGE}" >> "$LOG_FILE"
+# logging functions
+prepare_log() {    
+    local TYPE="$1"; shift
+	local TEXT="$*"; if [ "$#" -eq 0 ]; then TEXT="$(cat)"; fi
+	local DATETIME; DATETIME="$(date '+%Y-%m-%d %T')"
+    local LOGFILE; LOGFILE='/tmp/fszsfh.log'
+    echo "$DATETIME [$TYPE] $TEXT" | tee -a "$LOGFILE"
+	#printf '%s [%s] [Entrypoint]: %s\n' "$dt" "$TYPE" "$TEXT"
 }
 
-function _sshcmd() {
-    # WIP
-    local REMOTE_HOST
-
-    REMOTE_HOST=${1}
-
-    ssh -oUser=fnt -oHost="$REMOTE_HOST" -oPort=22
+log_info() {
+	prepare_log INFO "$@"
 }
 
-TSSHSOCK='/tmp/tmux-1000/sshtsock'
+log_warn() {
+	prepare_log WARN "$@" >&2
+}
+
+log_error() {
+	prepare_log ERROR "$@" >&2
+	exit 1
+}
+
+# global variables
+sock_name='ssh'
+sock_path="/tmp/tmux-1000/$sock_name"
 declare -A CHOICES
 
+# example of CHOICES
+CHOICES+=( [localhost]='127.0.0.1' )
+
+# fzf theme
 export FZF_DEFAULT_OPTS=$FZF_DEFAULT_OPTS'
   --color=fg:#dbcbcb,fg+:#ffffff,bg:#161616,bg+:#161616
   --color=hl:#5f87af,hl+:#5fd7ff,info:#e7e723,marker:#87ff00
@@ -41,42 +48,38 @@ export FZF_DEFAULT_OPTS=$FZF_DEFAULT_OPTS'
 
 if [ -e "$HOME/.fszsfh.txt" ]; then
     . "$HOME/.fszsfh.txt" && \
-        echo "Sourced $HOME/.fszsfh.txt " | _log "$@"
+        log_info "Sourced $HOME/.fszsfh.txt"
 fi
 
+# wip
 #test -n "$1" && echo "arg 1 is $1" | _log "$@" && exit 70
 if [ -n "$1" ]; then
-    echo "arg 1 is $1" | _log "$@" && exit 70
+    log_info "arg 1 is $1" && exit 70
     #CHOICE="$1"
 fi
-
-CHOICES+=( [localhost]='127.0.0.1' )
 
 if [ -z "$CHOICE" ]; then
     CHOICE=$(for i in "${!CHOICES[@]}"; do echo "$i"; done | fzf)
 fi
 
 if [ -n "$CHOICE" ]; then
-    echo Selected "${CHOICE} -> ${CHOICES[$CHOICE]}" | _log 'INFO'
+    log_info "Selected ${CHOICE} -> ${CHOICES[$CHOICE]}"
 else
-    echo "Choice is null exiting..." | _log 'WARN' && exit 71
+    log_warn "Choice is null exiting..." && exit 71
+fi
+
+tmuxpid=$(tmux -S "$sock_path" list-session -F "#{pid}" 2>/dev/null)
+
+if [ -z "$tmuxpid" ]; then
+    log_info "Create new-session -> $CHOICE"
+    tmux -L "$sock_name" -S "$sock_path" new-session -s "$CHOICE" "${CHOICES[$CHOICE]}" && exit 70
+fi
+
+if ! tmux -L "$sock_name" -S "$sock_path" has-session -t "$CHOICE" 2>/dev/null; then
+    log_info "No session exists create new-session -> $CHOICE"
+    tmux -L "$sock_name" -S "$sock_path" new-session -d -s "$CHOICE" "${CHOICES[$CHOICE]}" \; switch-client -t "$CHOICE" && exit 70
 fi
 
 
-if ! tmux -S $TSSHSOCK list-session -F "#{pid}" 2>/dev/null; then
-    echo "create init session" | _log
-    tmux -S $TSSHSOCK new-session -d -s "$CHOICE" "${CHOICES[$CHOICE]}"
-fi
-
-
-#if ! tmux -S $TSSHSOCK has-session -t "$CHOICE" 2>/dev/null; then
-#    echo "create a new session" | _log
-#    #tmux -S $TSSHSOCK attach-session -dx -t "${CHOICE}" && exit 70
-#    tmux -S $TSSHSOCK new-session -d -s "$CHOICE" "${CHOICES[$CHOICE]}"
-#fi
-
-#echo "attach to session" | _log
-#tmux -S $TSSHSOCK attach-session -dx \; new-session -d -s "$CHOICE" "${CHOICES[$CHOICE]}" \; attach-session -dx -t "$CHOICE"
-#tmux -S $TSSHSOCK attach-session -dx \; new-session -s "$CHOICE" "${CHOICES[$CHOICE]}" \; switch-client -t "$CHOICE" && exit 70
-#tmux -S $TSSHSOCK attach-session -dx -t "${CHOICE}"
-#switch-client -t "$CHOICE"
+log_info "attach to existing session -> $CHOICE"
+tmux -L "$sock_name" -S "$sock_path" attach-session -dx -t "$CHOICE"
